35f8a62f106d0dc03b3a66c9d070258ce603d671
Sebastian Agudelo <sebastamriel@gmail.com>
2024-10-16 15:45:40 -0500

    add null mapper objects

--- src/plugins/products/domain/repository/internal/mapper/product.mapper.ts
+++ src/plugins/products/domain/repository/internal/mapper/product.mapper.ts
@@ -5,22 +5,28 @@ import { ProductDTO } from "../../../model/dto/product.dto";
 export class ProductMapper {
 
     static mapToProductDTO(product: Product): ProductDTO {
+        if (product == null) {
+            return null;
+        }
         const productDTO = new ProductDTO();
-        productDTO.id = product._id.toString();
-        productDTO.name = product.name;
-        productDTO.description = product.description;
-        productDTO.price = product.price;
-        productDTO.stock = product.stock;
+        productDTO.id = product._id?.toString();
+        productDTO.name = product?.name;
+        productDTO.description = product?.description;
+        productDTO.price = product?.price;
+        productDTO.stock = product?.stock;
         return productDTO;
     }
 
     static mapToProduct(productDTO: ProductDTO): Product {
+        if (productDTO == null) {
+            return null;
+        }
         const product = new Product();
-        product._id = new Types.ObjectId(productDTO.id);
-        product.name = productDTO.name;
-        product.description = productDTO.description;
-        product.price = productDTO.price;
-        product.stock = productDTO.stock;
+        product._id = new Types.ObjectId(productDTO?.id);
+        product.name = productDTO?.name;
+        product.description = productDTO?.description;
+        product.price = productDTO?.price;
+        product.stock = productDTO?.stock;
         return product;
     }
 
--- src/plugins/sales/domain/repository/internal/mapper/sale-product.mapper.ts
+++ src/plugins/sales/domain/repository/internal/mapper/sale-product.mapper.ts
@@ -5,6 +5,9 @@ import { SaleProduct } from "../../../model/document/sale-product.document";
 export class SaleProductMapper {
 
     static mapToSaleProductDTO(saleProduct: SaleProductDTO): SaleProductDTO {
+        if (saleProduct == null) {
+            return null;
+        }
         const saleProductDTO = new SaleProductDTO();
         saleProductDTO.id = saleProduct.id;
         saleProductDTO.name = saleProduct.name;
@@ -14,6 +17,9 @@ export class SaleProductMapper {
     }
     
     static mapToSaleProduct(saleProductDTO: SaleProductDTO): SaleProduct {
+        if (saleProductDTO == null) {
+            return null;
+        }
         const saleProduct = new SaleProduct();
         saleProduct.idProducto = new Types.ObjectId(saleProductDTO.id);
         saleProduct.name = saleProductDTO.name;
