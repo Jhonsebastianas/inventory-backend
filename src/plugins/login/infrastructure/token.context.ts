f45abf5f7c37749f82e484a75e85f89bb46df324
Sebastian Agudelo <sebastamriel@gmail.com>
2024-10-18 18:09:15 -0500

    Add token in autorización

--- src/plugins/login/infrastructure/token.context.ts
+++ src/plugins/login/infrastructure/token.context.ts
@@ -9,6 +9,13 @@ export class TokenContext {
     constructor(@Inject(REQUEST) private request: Request) { }
 
     get tokenUser(): string {
-        return this.request.cookies[cookieConstants.token];
+        if (this.request.cookies[cookieConstants.token]) {
+            return this.request.cookies[cookieConstants.token];
+        }
+        const authHeader = this.request.headers['authorization'];
+        if (authHeader && authHeader.startsWith('Bearer ')) {
+            return authHeader.split(' ')[1];  // Retorna el token sin el "Bearer "
+        }
+        return null;
     }
 }
