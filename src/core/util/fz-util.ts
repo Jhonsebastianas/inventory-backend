b9a578396e01eba65c352a6ffb45fab16afc6d02
Sebastian Agudelo <sebastamriel@gmail.com>
2024-10-18 14:29:15 -0500

    memoryUsage method

--- src/core/util/fz-util.ts
+++ src/core/util/fz-util.ts
@@ -37,4 +37,19 @@ export class FzUtil {
     static genUUID() {
         return uuidv4()
     }
+
+    static printMemoryUsage() {
+        const formatMemoryUsage = (data: any) => `${Math.round(data / 1024 / 1024 * 100) / 100} MB`;
+
+        const memoryData = process.memoryUsage();
+
+        const memoryUsage = {
+            rss: `${formatMemoryUsage(memoryData.rss)} -> Resident Set Size - total memory allocated for the process execution`,
+            heapTotal: `${formatMemoryUsage(memoryData.heapTotal)} -> total size of the allocated heap`,
+            heapUsed: `${formatMemoryUsage(memoryData.heapUsed)} -> actual memory used during the execution`,
+            external: `${formatMemoryUsage(memoryData.external)} -> V8 external memory`,
+        };
+        
+        console.log(memoryUsage);
+    }
 }
